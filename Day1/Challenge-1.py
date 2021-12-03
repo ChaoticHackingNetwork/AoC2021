a2e5414f60db9f36810fda790927d9d929a3b86a
Chaotic Guru <49621391+Chaotic-Lab@users.noreply.github.com>
2021-12-03 06:03:49 +0000

    Challenge-1

--- Day1/Challenge-1.py
+++ Day1/Challenge-1.py
@@ -23,9 +23,12 @@ For example, suppose you had the following report:
 260
 263
 
-This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 199, 200, 208, 210, and so on.
+This report indicates that, scanning outward from the submarine, the sonar sweep found depths of 
+199, 200, 208, 210, and so on.
 
-The first order of business is to figure out how quickly the depth increases, just so you know what you're dealing with - you never know if the keys will get carried into deeper water by an ocean current or a fish or something.
+The first order of business is to figure out how quickly the depth increases, just so you know 
+what you're dealing with - you never know if the keys will get carried into deeper water by an 
+ocean current or a fish or something.
 
 To do this, count the number of times a depth measurement increases from the previous measurement.
 """
